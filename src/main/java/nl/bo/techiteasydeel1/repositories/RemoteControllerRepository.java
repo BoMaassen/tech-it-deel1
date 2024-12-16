54a0366964aa1132f53503a4eec27227fcbc7743
BoMaassen <bomaassen@live.nl>
2024-12-16 16:22:51 +0100

    Repositories aangemaakt

--- src/main/java/nl/bo/techiteasydeel1/repositories/RemoteControllerRepository.java
+++ src/main/java/nl/bo/techiteasydeel1/repositories/RemoteControllerRepository.java
@@ -1,12 +1,9 @@
 package nl.bo.techiteasydeel1.repositories;
 
-import nl.bo.techiteasydeel1.models.Television;
+import nl.bo.techiteasydeel1.models.RemoteController;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface TelevisionRepository extends JpaRepository<Television, Long> {
-
+public interface RemoteControllerRepository extends JpaRepository<RemoteController, Long> {
 }
