5dbfaea2769d38f5d74b37866c9cfa23b546721f
BoMaassen <bomaassen@live.nl>
2024-12-17 10:36:46 +0100

    Dtos af

---
+++ src/main/java/nl/bo/techiteasydeel1/dtos/WallBracketDto.java
@@ -0,0 +1,49 @@
+package nl.bo.techiteasydeel1.dtos;
+
+public class WallBracketDto {
+    private Long id;
+    private String size;
+    private Boolean adjustable;
+    private String name;
+    private double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public Boolean getAdjustable() {
+        return adjustable;
+    }
+
+    public void setAdjustable(Boolean adjustable) {
+        this.adjustable = adjustable;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
